f3e3f7d0dfbb248938af794e7d4aeb74f8f2ca89
RalenskiDoucet <Ralenskid@gmail.com>
2017-09-11 17:40:57 -0500

    commit

---
+++ All Classwork/Example Structs.cpp
@@ -0,0 +1,127 @@
+#include <iostream>
+
+	//struct Player
+	//{
+		//int mHealth;
+		//int mReasource;
+		//int mStrenght;
+		//int mLevel;
+		//float mMovementSpeed;
+		//int mArmor;
+	//};
+	//Player CreatePlayer();
+//	Player CreatePlayer(int health, int strenght, float movementSpeed);
+
+//int main()
+//{
+	//Player Ralenski= CreatePlayer();
+	//Player newPlayer = CreatePlayer(100, 10, 5);
+
+//}
+
+//Player CreatePlayer()
+//{
+	//Player newPlayer = {};
+	//return newPlayer;
+//}
+//layer CreatePlayer(int health, int strenght, float movementSpeed)
+//{
+//	return Player();
+//}
+
+//Player CreatePlayer(int health, int strenght, float movementSpeed);
+//}
+
+//struct FightClub{7}
+
+//FightClub [2];[200]
+//FightClub [3];[300]
+//FightClub [4];[400]
+//FightClub [5];[500]
+//FightClub [6];[600]
+//FightClub [7];[700]
+
+
+
+
+struct Player {};
+
+{
+	char mName;
+	int mScore;
+	std::cout << "Choose a name for your character", std end|
+		std::cin >>newPlayername ;
+	std::cout << ;
+		std::cin >> ;
+		std::cout << ;
+
+	return Player
+};
+
+
+ //struct Vehicle
+//	 void Carinfo(Vehicle )
+// { 
+//	int mNumberoftires=4;
+	//int mNumberofseats=4;
+	//char mMakername[255];
+	//char mModelname[255];
+	//int mModeyear=2006;
+
+//	Vehicle Matthew Nissan Frontier;
+//	Vehicle Luke Subura BRZ;
+//	Vehicle T.j Nissan Centra;
+//	Vehicle Zack Ford Focas;
+//	Vehicle Ralenski Chevy Impala;
+//};
+
+struct Vehicle
+{
+	int mNumbeeoftires;
+	int mNumberofdoors;
+	char mCarmaker[255];
+	char mModelname[255];
+	int mYearofmake;
+};
+
+struct item
+{
+	int mName;
+	int mCost;
+	int mQuanity;
+};
+
+struct shop
+{
+	int mTotalgold;
+	int mItem;
+};
+void std::cout<<(m.Totalgold);
+
+// Create a program that allows the user to search for Car and Year the car was made.
+
+struct Cars
+	[
+
+
+	]
+int main()
+{
+	Vehicle ebayAutomotive[15];
+	ebayAutomotive[0] = { 2, 4, "Porshe", "911", 2016 };
+	ebayAutomotive[1] = { 2, 4, "McLaren", "720s", 2018 };
+	ebayAutomotive[2] = { 4, 4, "Honda", "Civic", 2017 };
+	ebayAutomotive[3] = { 4,4,"Nissan", "Sentra", 2012 };
+	ebayAutomotive[4] = { 2,4, "Ford", "Mustang", 1965 };
+	ebayAutomotive[5] = { 2, 4, "Audi", "S8", 2017 };
+	ebayAutomotive[6] = { 2, 2, "Ford", "GT", 2017 };
+	ebayAutomotive[7] = { 2,4,"Toyota", "Supra", 1998 };
+	ebayAutomotive[8] = { 4,4,"Ford", "Fiesta", 2005 };
+	ebayAutomotive[9] = { 2, 1, "YuGiOh", "Duel Runner", 2025 };
+	ebayAutomotive[10] = { 2, 3, "UNSC", "Warthog", 2500 };
+	ebayAutomotive[11] = { 2, 1, "Moongoose", "Legion L40", 2017 };
+	ebayAutomotive[12] = { 2, 2, "Ferrari", "La Farrari", 2015 };
+	ebayAutomotive[13] = { 2, 2, "TVR", "Sergis", 2013 };
+	ebayAutomotive[14] = { 1, 0, "US Army", "M1 Abrams", 2018 };
+
+}
