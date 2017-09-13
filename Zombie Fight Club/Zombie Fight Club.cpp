42fefc195ac10a13104b96ceea4284a71de39a8a
RalenskiDoucet <Ralenskid@gmail.com>
2017-09-13 12:41:36 -0500

    commit

--- Zombie Fight Club/Zombie Fight Club.cpp
+++ Zombie Fight Club/Zombie Fight Club.cpp
@@ -1,10 +1,6 @@
 #include "Zombie Fight Club.h"
 #include "Zombie Fight Club.h"
 
-void Zombies::LoseHealthWhenAttacked(mHealth, mAttackPower)
-{
-}
-
 void Zombies::LoseHealth(int mHealth, int mAttackPower)
 {
 	return LoseHealth(mHealth, -mAttackPower);
